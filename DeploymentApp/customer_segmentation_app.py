67c12ea301440bfeeb7e06d0780592e283a04455
amir <as.daoudi@esi-sba.dz>
2024-01-13 19:41:28 +0100

    V1 web App.

--- DeploymentApp/customer_segmentation_app.py
+++ DeploymentApp/customer_segmentation_app.py
@@ -55,6 +55,83 @@ def main():
     
     st.success(result)
     
+    
+html_str1 = """# <p style="background-color:#682F2F;font-family:newtimeroman;color:#FFF9ED;font-size:150%;text-align:center;border-radius:10px 10px;">Customer Segmentation</p>
+
+<img src="https://github.com/KarnikaKapoor/Files/blob/main/Colorful%20Handwritten%20About%20Me%20Blank%20Education%20Presentation.gif?raw=true"  alt="Centered Image" style=" max-width: 100%;
+      max-height: 100%;">
+
+In this project, I will be performing an unsupervised clustering of data on the customer's records from a groceries firm's database.
+Customer segmentation is the practice of separating customers into groups that reflect similarities among customers in each cluster.
+I will divide customers into segments to optimize the significance of each customer to the business.
+To modify products according to distinct needs and behaviours of the customers.
+It also helps the business to cater to the concerns of different types of customers.
+"""    
+html_str2 =  """ <h3>The Study Outcome</h3>
+
+    <section>
+        <h2>Cluster 0: </h2>
+        <ul>
+            <li>Average Income of $34,865 yearly.</li>
+            <li>Average Spending is $500.</li>
+            <li>The majority of them have not accepted any promotions yet (822).</li>
+            <li>Most of them have completed purchases using a discount half of the time (1/2).</li>
+            <li>Have either 1 or 2 children.</li>
+            <li>Their age ranges between 25 and 70.</li>
+            <li>Are at the graduate, postgraduate, or undergraduate level.</li>
+            <li>Most are married, only a few are unmarried.</li>
+            <li>Most of them are parents, very few are not parents.</li>
+        </ul>
+    </section>
+
+    <section>
+        <h2>Cluster 1: </h2>
+        <ul>
+            <li>Average Income of $65,463 yearly.</li>
+            <li>Average Spending is between $550 and $2000.</li>
+            <li>The majority of them have not accepted any promotions yet (428).</li>
+            <li>Most of them have completed purchases using a discount a quarter of the time (1/4).</li>
+            <li>Most of them have one child.</li>
+            <li>Their age ranges between 35 and 70.</li>
+            <li>Are at the graduate or postgraduate level.</li>
+            <li>Most of them are married.</li>
+            <li>Are not parents.</li>
+        </ul>
+    </section>
+
+    <section>
+        <h2>Cluster 2: </h2>
+        <ul>
+            <li>Average Income of $78,413 yearly.</li>
+            <li>Average Spending is between $750 and $2250.</li>
+            <li>The majority of them have not accepted any promotions yet (210).</li>
+            <li>Most of them have completed purchases using a discount half of the time (1/2).</li>
+            <li>Don't have any children.</li>
+            <li>Their age ranges between 40 and 70.</li>
+            <li>Are at the graduate, and very few are at the postgraduate level.</li>
+            <li>Most of them are married.</li>
+            <li>Are parents.</li>
+        </ul>
+    </section>
+
+    <section>
+        <h2>Cluster 3:</h2>
+        <ul>
+            <li>Average Income of $45,902 yearly.</li>
+            <li>Average Spending is between $0 and $1000.</li>
+            <li>The majority of them have not accepted any promotions yet (297).</li>
+            <li>Most of them have completed purchases using a discount 3 to 5 times.</li>
+            <li>Have one, two, or three children.</li>
+            <li>Their age ranges between 40 and 70.</li>
+            <li>All of them are at the graduate and postgraduate level.</li>
+            <li>Most of them are married.</li>
+            <li>Are parents.</li>
+        </ul>
+    </section>"""
+
+st.markdown(html_str1, unsafe_allow_html= True)    
+st.markdown(html_str2, unsafe_allow_html= True)    
+
 
 if __name__ == '__main__':
         main ()
